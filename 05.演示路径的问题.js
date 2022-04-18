767dfad577c196d4155420d308ff2e2e8a96e475
wjq <1092229612@qq.com>
2022-04-18 20:05:21 +0800

    20220418-1

---
+++ 05.演示路径的问题.js
@@ -0,0 +1,20 @@
+// const fs = require('fs')
+
+// fs.readFile('./files/1.txt', 'utf8', function (err, dataStr) {
+//     if (err) {
+//         return console.log('读取文件失败! ' + err.message);
+//     }
+//     console.log('读取文件成功! ' + dataStr);
+// })
+
+// const fs = require('fs')
+
+// fs.readFile('D:\\web\\Node.js\\files\\1.txt', 'utf8', function (err, dataStr) {
+//     if (err) {
+//         return console.log('读取文件失败! ' + err.message);
+//     }
+//     console.log('读取文件成功! ' + dataStr);
+// })
+
+// 当前文件所处的目录
+console.log(__dirname);
